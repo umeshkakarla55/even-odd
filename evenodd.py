1c8cc63362bba85a81517a31d1adedf2a7a1c859
umeshkakarla55 <umeshkakarla33@gmail.com>
2026-09-18 16:39:29 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,4 @@
+#check whether the given number is even or odd
+#if the number is exactly divisible by 2 and gives remainder as 0 then print even.
+#if the number gives remainder as 1 print odd.
+num=int(input())
